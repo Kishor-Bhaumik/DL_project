b40566a579ce769bdd04734d337cb8de8129f4dc
Kishor Kumar Bhaumik <kishorebd2500@gmail.com>
2024-11-24 22:05:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ import torchvision.models as models
 from collections import defaultdict
 from data_proc import prepare_imagenet
 
+
+# torch.backends.cudnn.enabled = False
+
+
 parser = argparse.ArgumentParser(description='PyTorch Tiny ImageNet Training')
 
 parser.add_argument('--dataset', default='tiny-224', choices=['mnist', 'tiny-224'],  help='name of dataset to train on (default: tiny-imagenet-200)')
 parser.add_argument('--data-dir', default=os.getcwd(), type=str,   help='path to dataset (default: current directory)')
-parser.add_argument('--batch-size', default=1000, type=int,   help='mini-batch size for training (default: 1000)')
-parser.add_argument('--test-batch-size', default=1000, type=int,   help='mini-batch size for testing (default: 1000)')
+parser.add_argument('--batch-size', default=128, type=int,   help='mini-batch size for training (default: 1000)')
+parser.add_argument('--test-batch-size', default=128, type=int,   help='mini-batch size for testing (default: 1000)')
 parser.add_argument('--epochs', default=25, type=int,   help='number of total epochs to run (default: 25)')
 parser.add_argument('--seed', default=1, type=int,      help='seed for initializing training (default: 1)')
 parser.add_argument('--no-cuda', action='store_true',   help='run without cuda (default: False)')
@@ -42,12 +46,95 @@ parser.add_argument('--models-dir', default=os.path.join(os.getcwd(), 'models'),
 parser.add_argument('--load', action='store_true',  help='load model (default: False)')
 parser.add_argument('--model-path', default=os.path.join(os.getcwd(), 'models', 'default.pt'), type=str,  help='path to load model (default: cwd/models/default.pt)')
 parser.add_argument('--err', action='store_true',  help='plot error analysis graphs (default: False)')
+parser.add_argument('--use_wandb', action='store_true', default= False,   help='log with wandb ')
 
 args = parser.parse_args()
+if not args.no_cuda:
+    torch.cuda.manual_seed(args.seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 train_loader, test_loader = prepare_imagenet(args)
 
 
-for img , label in train_loader:
-    print(img.shape); exit()
+model = models.resnet18(weights=False)
+model.fc = nn.Linear(512, 200)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+experiment = 'resent18'
+if args.use_wandb:
+    import wandb
+    wandb.init(project="finalProj_DL", name=experiment)
+    wandb.require("core")
 
 
+def init_weights(m):
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+
+model.apply(init_weights)
+
+# Test the model
+def test(model, test_loader):
+    model.eval()
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+
+            correct += predicted.eq(labels).sum().item()
+    
+    acc = correct / total
+    return acc
+
+
+def train(model, train_loader, criterion, optimizer, num_epochs=args.epochs):
+    model.train()
+    
+    train_losses = []
+    train_accs = []
+    test_accs = []
+
+        
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+        
+        epoch_loss = running_loss / len(train_loader)
+        epoch_acc = correct / total
+        test_acc = test(model, test_loader)
+        
+        train_losses.append(epoch_loss)
+        train_accs.append(epoch_acc)
+        test_accs.append(test_acc)
+        
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}, Train Acc: {epoch_acc:.4f}, Test Acc: {test_acc:.4f}")
+
+        if args.use_wandb:
+            wandb.log({"Train Accuracy :": epoch_acc, "Test Accuracy :": test_acc, "Train loss": epoch_loss})
+        
+    return train_losses, train_accs, test_accs
+
+
+train_losses, train_accs, test_accs = train(model.to(device), train_loader, criterion, optimizer)
